e2ff098bce069e7f18608b61f99055fa0123889b
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2022-08-18 21:38:01 +0000

    add employee model

---
+++ frontend/src/app/models/employee.ts
@@ -0,0 +1,17 @@
+export interface EmployeeModel {
+  id : string;
+  fullName: string;
+  email:string;
+  birthDate:Date;
+  adresse:string;
+  nationality:string;
+  religion:string;
+  martialStatus:string;
+  passportNumber:string;
+  emergencyContact:string;
+  bankName:string;
+  accountNumber:string;
+  IFSC:string;
+  PAN:string;
+  UPI:string;
+}
