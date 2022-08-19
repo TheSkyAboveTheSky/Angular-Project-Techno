ad6b77848858644131774fdda263b9b318ea80ad
TheSkyAboveTheSky <94720343+TheSkyAboveTheSky@users.noreply.github.com>
2022-08-19 06:19:57 +0000

    personal popup v1.2

--- front/src/app/components/edit-personal-informations/edit-personal-informations.component.ts
+++ front/src/app/components/edit-personal-informations/edit-personal-informations.component.ts
@@ -3,17 +3,18 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from 'src/app/material-module';
 import { EmployeeService } from 'src/app/services/employee.service';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-personal-informations',
   standalone: true,
-  imports: [CommonModule,MaterialModule],
+  imports: [CommonModule,MaterialModule,FormsModule,ReactiveFormsModule],
   templateUrl: './edit-personal-informations.component.html',
   styleUrls: ['./edit-personal-informations.component.css']
 })
 export class EditPersonalInformationsComponent implements OnInit {
-
   constructor(public dialogRef : MatDialogRef<EditPersonalInformationsComponent>, private service : EmployeeService) { }
+  myform = this.service.myform;
 
   ngOnInit(): void {
   }
@@ -21,18 +22,26 @@ export class EditPersonalInformationsComponent implements OnInit {
   {
     if (this.service.myform.valid)
     {
-      this.service.updateEmployee(this.service.myform.value._id,this.service.myform.value).subscribe(
+      this.service.updateEmployee(this.service.myform.value.id,this.service.myform.value).subscribe(
         (response) => {
           console.log(response);
         },
         (error) => {
           console.log(error);
+        },
+        ()=>{
+          this.onClose();
+          window.location.reload();
         }
       );
     }
     else {
       console.log("Error updating !");
     }
+  }
+  onClose()
+  {
+    this.dialogRef.close();
   }
 
 }
